f1e11888c90f33bb132b5007adca5e750788bd0e
itsmrma <m.benassi30@gmail.com>
2026-09-10 20:39:38 +0200

    fix responsiveness

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@-oju(a%t^3kldk_mk=eh+1p%v(=1$m(jh=usg&u&4-m-hr!ad'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-@-oju(a%t^3kldk_mk=eh+1p%v(=1$m(jh=usg&u&4-m-hr!ad')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
